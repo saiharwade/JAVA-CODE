591669bc10d82014b2888ecaeb7399fdcc5c80c5
Sai Harwade <saiharwade12@gmail.com>
2026-04-11 18:52:56 +0530

    string basic

--- String_Class/src/Main.java
+++ String_Class/src/Main.java
@@ -1,13 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+class Sting_class{
+    public static void main(){
+        //string are always count in objects, but when we store it as string var1 = "" it can be compared with the other string variable which is declared in the same way.
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        //ex
+
+        String new1 = "hello1";
+        String new2 = "hello";
+        if(new1 == new2){
+            System.out.println("true");
+        }
+        //if both variable have same value then they point to the same reference.
+        //but these normally declared can only be compared with ==.
+        // other for ex
+        //if we declared strings like proper objects
+
+        String ex1 = new String("world");
+        String ex2 = new String("world");
+
+        if(ex1.equals(ex2)){
+            System.out.print("true");
+        }
+        // so here when we compare the two objects then we have to write with using equals() method.
     }
 }
