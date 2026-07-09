2fbca1194665bd83a5e394d30bb81edf88c6b4e9
Sai Harwade <saiharwade12@gmail.com>
2026-07-09 19:48:03 +0530

    Learned about Thread class and Runnable interface

---
+++ OOP_JAVA/oop/src/MultiThreading/Runnable/RunnableTesting.java
@@ -0,0 +1,29 @@
+package MultiThreading.Runnable;
+
+public class RunnableTesting {
+    public static void main(String[]args){
+
+        long startTime = System.currentTimeMillis();
+
+        PrintTask p1 = new PrintTask('*');
+        PrintTask p2 = new PrintTask('#');
+        PrintTask p3 = new PrintTask('$');
+
+        Thread t1 = new Thread(p1);//Although we created the p1 object the class it implements the runnable interface no class to extend its method
+        t1.setPriority(Thread.MIN_PRIORITY);//THIS TELL THE SYSTEM WHICH TASK SHOULD BE GIVEN MORE PRIORITY
+        t1.start(); //So we have to pass the p1 object to the t1 object which makes the p1 object run.
+
+        Thread t2 = new Thread(p2);
+        t2.setPriority(Thread.MAX_PRIORITY);
+        t2.start();
+
+        Thread t3 = new Thread(p3);
+        t3.setPriority(5); // you also give the number between, 1 to 10.
+        t3.start();
+
+        long endTime = System.currentTimeMillis();
+        System.out.printf("%s Total time taken: %d",
+                Thread.currentThread().getName(),
+                (endTime - startTime));
+    }
+}
